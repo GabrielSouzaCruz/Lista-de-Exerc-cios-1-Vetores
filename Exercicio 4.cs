7955ad1d94660301e3e61175822287de5097f271
GabrielSouzaCruz <contato.gabrielsouzacruz@gmail.com>
2024-10-12 23:18:05 -0300

    Add files via upload

---
+++ Exercicio 4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Arrays;
+
+namespace Exercicio4
+{
+    class Execicio_4
+    {
+        static void Main()
+        {
+            int n;
+
+            Console.Write("Entre com o número de Elementos: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int[] meuVetor = new int[n];
+
+            BibliotecaArray.geraVetor(meuVetor);
+
+            Console.WriteLine("O elemento digitados foram: ");
+            BibliotecaArray.mostraVetor(meuVetor);
+
+            Console.WriteLine();
+
+            BibliotecaArray.imparesElemento(meuVetor);
+
+            Console.ReadKey();
+        }
+    }
+}
