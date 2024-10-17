8275aa755969606846dd207c146b30d5c42c6605
Gabriel de Souza Cruz <contato.gabrielsouzacruz@gmail.com>
2024-10-17 10:14:19 -0300

    Add files via upload

---
+++ Exercicio 11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Arrays;
+
+namespace Exercicio11
+{
+    class Exercicio_11
+    {
+        static void Main()
+        {
+            string frase;
+
+            Console.Write("Entre com Uma Frase Codificada: ");
+            frase = Console.ReadLine();
+
+            for (int i = 1; i < frase.Length-1; i++)
+            {
+               
+                if (frase[i] != 'p' && frase[i] != 'p') 
+                {
+                    Console.Write(frase[i]);
+                }else if (frase[i-1] == 'p' && frase[i+1] == 'p')
+                {
+                    Console.Write(frase[i]);
+                }
+            }
+            Console.Write(frase[frase.Length - 1]);
+        }
+    }
+}
