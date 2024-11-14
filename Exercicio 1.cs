ffefff9285526c2d77a1dae04cc71d76d6eee9e2
Gabriel de Souza Cruz <contato.gabrielsouzacruz@gmail.com>
2024-11-14 15:29:09 -0300

    Update Exercicio 1.cs

--- Exercicio 1.cs
+++ Exercicio 1.cs
@@ -1,1 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Arrays;
+namespace Exercicio1
+{
+    class Exercicio_1
+    {
+        static void Main()
+        {
+            int n;
+           
+            Console.Write("Entre com o número de Elementos: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int[] meuVetor = new int[n];
+
+            BibliotecaArray.geraVetor(meuVetor);
+
+            Console.WriteLine("O elemento digitados foram: ");
+            BibliotecaArray.mostraVetor(meuVetor);
+            Console.WriteLine();
+
+            BibliotecaArray.somaVetor(meuVetor);
+
+            Console.ReadKey();
+        }
+    }
+}
 
